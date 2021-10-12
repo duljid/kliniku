8468c63b8349e1270b46e2f0ca200cb2c6e20eae
notariush24 <notariusharefa00@gmail.com>
2021-10-12 13:20:11 +0700

    CRUD Apotek Done

--- app/Controllers/Apotek.php
+++ app/Controllers/Apotek.php
@@ -8,7 +8,116 @@ use App\Models\ModelApotek;
 class Apotek extends BaseController
 {
     public function index()
-    {     
-        return view('Apotek/index');
+    {
+        session();
+        $model = new ModelApotek();
+        $data = [
+            'obat' => $model->findAll(),
+        ];
+
+        return view('Apotek/index', $data);
+    }
+
+    public function tambah()
+    {
+        $model = new ModelApotek();
+        $cekKode = $model->selectMax('kode_obat')->first();
+        
+        $kodeOtomatis = '';
+        if(!empty($cekKode)){
+			$tmp = Substr($cekKode['kode_obat'], 2) + 1;
+			$kodeOtomatis = 'KO'.sprintf('%04s', $tmp);
+		}
+		else{
+			$kodeOtomatis = 'KO'.'0001';
+		}
+
+        session();
+        $data = [
+            'kodeOtomatis' => $kodeOtomatis,
+            'validation' => \Config\Services::validation()
+        ];
+
+        return view('Apotek/tambah', $data);
+    }
+
+    public function insert()
+    {
+        if (!$this->validate([
+			'nama_obat' => 'required',
+			'satuan_obat' => 'required',
+			'jenis_obat' => 'required',
+			'stok_obat' => 'required|greater_than_equal_to[10]',
+			'tgl_exp' => 'required',
+        ])) {
+            return redirect()->to(base_url('/apotek/tambah'))->withInput();
+        }
+
+        $data = $this->request->getVar();
+        
+        $model = new ModelApotek();
+        $model->save([
+            'kode_obat' => $data['kode_obat'],
+            'nama_obat' => $data['nama_obat'],
+            'satuan_obat' => $data['satuan_obat'],
+            'jenis_obat' => $data['jenis_obat'],
+            'stok_obat' => $data['stok_obat'],
+            'tgl_exp' => $data['tgl_exp'],
+            'kegunaan' => $data['kegunaan'],
+        ]);
+
+        return redirect()->to(base_url('/apotek'));
+    }
+
+    public function ubah($kodeObat)
+    {
+        session();
+        $model = new ModelApotek();
+        $data = [
+            'obat' => $model->where('kode_obat', $kodeObat)->first(),
+            'validation' => \Config\Services::validation()
+        ];
+
+        return view('apotek/ubah', $data);
+    }
+
+    public function update($idObat)
+    {
+        $data = $this->request->getVar();
+
+        if (!$this->validate([
+			'nama_obat' => 'required',
+			'satuan_obat' => 'required',
+			'jenis_obat' => 'required',
+			'stok_obat' => 'required|greater_than_equal_to[10]',
+			'tgl_exp' => 'required',
+        ])) {
+            return redirect()->to(base_url('/apotek/ubah/'.$data['kode_obat']))->withInput();
+        }
+        
+        $model = new ModelApotek();
+        $model->save([
+            'id_obat' => $idObat,
+            'kode_obat' => $data['kode_obat'],
+            'nama_obat' => $data['nama_obat'],
+            'satuan_obat' => $data['satuan_obat'],
+            'jenis_obat' => $data['jenis_obat'],
+            'stok_obat' => $data['stok_obat'],
+            'tgl_exp' => $data['tgl_exp'],
+            'kegunaan' => $data['kegunaan'],
+        ]);
+
+        return redirect()->to(base_url('/apotek'));
+    }
+
+    public function hapus($kodeObat)
+    {
+        $model = new ModelApotek();
+        $obat = $model->where('kode_obat', $kodeObat)->first();
+        $idObat = $obat['id_obat'];
+
+        $model->delete(['id_obat', $idObat]);
+
+        return redirect()->to(base_url('/apotek'));
     }
 }
--- app/Views/Apotek/index.php
+++ app/Views/Apotek/index.php
@@ -1,9 +1,50 @@
 <?= $this->extend('template/index'); ?>
 
 <?= $this->Section('content');?>
+<?php $no = 1; ?>
 <div class="m-4 apotek">
     <section>
         <h1 class="h3 mb-4 text-gray-800">Data Obat</h1>
     </section>
+    <div class="card">
+        <div class="card-header">
+            <a href="<?= base_url('apotek/tambah'); ?>" class="btn btn-primary btn-sm"><i class="fa fa-plus"></i> <b>Tambah Data</b></a>
+        </div>
+        <div class="card-body">
+            <table class="table table-bordered" id="data-list">
+                <thead>
+                    <tr align="center">
+                        <td width="5%">No</td>
+                        <td>Kode</td>
+                        <td>Nama</td>
+                        <td>Satuan</td>
+                        <td>Jenis</td>
+                        <td>Stok</td>
+                        <td>Tanggal Kadaluarasa</td>
+                        <td>Kegunaan</td>
+                        <td width="10%">Opsi</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php foreach ($obat as $row) { ?>
+                        <tr align="center">
+                            <td><?= $no++; ?></td>
+                            <td><?= $row['kode_obat']; ?></td>
+                            <td><?= $row['nama_obat']; ?></td>
+                            <td><?= $row['satuan_obat']; ?></td>
+                            <td><?= $row['jenis_obat']; ?></td>
+                            <td><?= $row['stok_obat']; ?></td>
+                            <td><?= $row['tgl_exp']; ?></td>
+                            <td><?= $row['kegunaan']; ?></td>
+                            <td>
+                                <a href="<?= base_url('/apotek/ubah/'.$row['kode_obat']); ?>" class="btn btn-sm btn-warning"><i class="fa fa-edit"></i></a>
+                                <a href="<?= base_url('/apotek/hapus/'.$row['kode_obat']); ?>" class="btn btn-sm btn-danger"><i class="fa fa-trash"></i></a>
+                            </td>
+                        </tr>
+                    <?php } ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
 </div>
 <?= $this->endSection(); ?>
